11a3b9c401773bba8ba3513f617c4d684d5b0ab9
unknown <hh727w@gmail.com>
2025-05-12 11:00:43 -0700

    update tailwind for newer version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('@tailwindcss/postcss').Config} */
 module.exports = {
   darkMode: ["class"],
   content: [
@@ -87,5 +87,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [],
 } 
